9e9010767f19d97bc7dab56c56b777d7198bf6b3
luc-dev10 <luc.dev1008@gmail.com>
2022-02-19 20:58:41 -0500

    edit controller

--- src/main/java/com/lucio/springboot/rest/HomeController.java
+++ src/main/java/com/lucio/springboot/rest/HomeController.java
@@ -1,7 +1,6 @@
 package com.lucio.springboot.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,4 +11,9 @@ public class HomeController {
         return "Hello World";
     }
 
+    @GetMapping("users")
+    public String getUsers() {
+        return "hello";
+    }
+    
 }
